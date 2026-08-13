0177e46c5b3223e353f911878598420e067ea2e6
balaji811 <kamisettybalaji2@gmail.com>
2026-08-14 01:23:19 +0530

    Update ProfileController.java

--- projectSpring/src/main/java/com/example/projectSpring/controller/ProfileController.java
+++ projectSpring/src/main/java/com/example/projectSpring/controller/ProfileController.java
@@ -11,7 +11,7 @@ import com.example.projectSpring.model.Employee;
 import com.example.projectSpring.service.ProfileService;
 
 @RestController
-@CrossOrigin(origins = "http://127.0.0.1:5500")
+@CrossOrigin(origins = "https://employee-frontend-mocha-sigma.vercel.app")
 public class ProfileController {
 	
 	@Autowired
